3897bd83043fca1df65e46f09ecbbd2e0c9de1f7
pivolan <pivolan@gmail.com>
2016-02-17 00:53:44 +0400

    Созданы события. Сделано несколько подписчиков на события. Добавлена проверка на возможность опубликовать Обзор Добавлена таблица с решениями Куратора по поводу публикации. migration

---
+++ app/DoctrineMigrations/Version20160216235255.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160216235255 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE curator_decision (id INT AUTO_INCREMENT NOT NULL, curator_id INT DEFAULT NULL, product_id INT DEFAULT NULL, status VARCHAR(255) NOT NULL, reject_reason VARCHAR(4000) NOT NULL, updated_at DATETIME NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_4B340062733D5B5D (curator_id), INDEX IDX_4B3400624584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE curator_decision ADD CONSTRAINT FK_4B340062733D5B5D FOREIGN KEY (curator_id) REFERENCES fos_user (id)');
+        $this->addSql('ALTER TABLE curator_decision ADD CONSTRAINT FK_4B3400624584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE product ADD reserved_at DATETIME NOT NULL COMMENT \'Дата резервации экспертом\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE curator_decision');
+        $this->addSql('ALTER TABLE product DROP reserved_at');
+    }
+}
