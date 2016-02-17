4ac33d5131ad508f2a0e39671cd322bb7d51adc1
pivolan <pivolan@gmail.com>
2016-02-18 03:58:54 +0400

    on delete set null for curator in user

---
+++ app/DoctrineMigrations/Version20160218025802.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160218025802 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_user DROP FOREIGN KEY FK_957A6479733D5B5D');
+        $this->addSql('ALTER TABLE fos_user ADD CONSTRAINT FK_957A6479733D5B5D FOREIGN KEY (curator_id) REFERENCES fos_user (id) ON DELETE SET NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_user DROP FOREIGN KEY FK_957A6479733D5B5D');
+        $this->addSql('ALTER TABLE fos_user ADD CONSTRAINT FK_957A6479733D5B5D FOREIGN KEY (curator_id) REFERENCES fos_user (id)');
+    }
+}
