cc3dd83daf38154ff4976ec339ef98b79be822fe
pivolan <pivolan@gmail.com>
2016-02-22 19:15:39 +0400

    ProductType form tests

--- src/Exprating/ExpertBundle/Tests/Form/ProductTypeTest.php
+++ src/Exprating/ExpertBundle/Tests/Form/ProductTypeTest.php
@@ -27,21 +27,25 @@ class ProductTypeTest extends AbstractWebCaseTest
      */
     public function testSubmitValidData($data)
     {
+        $em = $this->doctrine->getManager();
         $category = new Category();
         $category->setSlug($data['categorySlug'])
             ->setName($data['categorySlug']);
+        $em->persist($category);
         $ratingSettings = new RatingSettings();
         $ratingSettings->setCategory($category)
             ->setRating1Label($data['label1'])
             ->setRating2Label($data['label2'])
             ->setRating3Label($data['label3'])
             ->setRating4Label($data['label4']);
+        $em->persist($ratingSettings);
         $category->setRatingSettings($ratingSettings);
         $product = new Product();
         $product->setCategory($category)
+            ->setSlug('wrwer')
             ->setName($data['product']['name'])
             ->setMinPrice($data['product']['minPrice']);
-
+        $em->persist($product);
         foreach ($data['characteristics'] as $name => $characteristicData) {
             $characteristic = new Characteristic();
             $characteristic->setName($name)
@@ -51,37 +55,50 @@ class ProductTypeTest extends AbstractWebCaseTest
                 ->setScale($characteristicData['scale']);
             $productCharacteristic = new ProductCharacteristic();
             $productCharacteristic->setProduct($product)
-                ->setCharacteristic($characteristic);
+                ->setCharacteristic($characteristic)
+                ->setValue($characteristicData['value']);
             $product->addProductCharacteristic($productCharacteristic);
+            $em->persist($characteristic);
+            $em->persist($productCharacteristic);
         }
-        $product2 = clone $product;
+        foreach ($data['new_characteristics'] as $name => $characteristicData) {
+            $characteristic = new Characteristic();
+            $characteristic->setName($name)
+                ->setType($characteristicData['type'])
+                ->setLabel($characteristicData['label'])
+                ->setSlug($name)
+                ->setScale($characteristicData['scale']);
+            $em->persist($characteristic);
+        }
 
-        $form = $this->client->getContainer()->get('form.factory')->create(ProductType::class, clone $product2);
+        $form = $this->client->getContainer()->get('form.factory')->create(ProductType::class, $product);
 
         // submit the data to the form directly
-        $form->submit($data['formData']);
+        $formData = $data['formData'];
+        $form->submit($formData);
 
         $this->assertTrue($form->isSynchronized());
 
-        foreach ($product->getProductCharacteristics() as $characteristic) {
-            $characteristic->setValue($data['characteristics'][$characteristic->getCharacteristic()->getSlug()]);
+
+        $this->assertEquals($formData['rating1'], $product->getRating1());
+        $this->assertEquals($formData['rating2'], $product->getRating2());
+        $this->assertEquals($formData['rating3'], $product->getRating3());
+        $this->assertEquals($formData['rating4'], $product->getRating4());
+
+        $this->assertEquals($formData['advantages'], $product->getAdvantages());
+        $this->assertEquals($formData['disadvantages'], $product->getDisadvantages());
+        $this->assertEquals($formData['expertOpinion'], $product->getExpertOpinion());
+        $this->assertEquals($formData['expertComment'], $product->getExpertComment());
+
+        $this->assertEquals(4, $product->getProductCharacteristics()->count());
+        foreach($product->getProductCharacteristics() as $productCharacteristic){
+            $this->assertContains($productCharacteristic->getValue(), [1001, 'u', 2.58, 3.58]);
         }
-        $product
-            ->setRating1($data['formData']['rating1'])
-            ->setRating2($data['formData']['rating2'])
-            ->setRating3($data['formData']['rating3'])
-            ->setRating4($data['formData']['rating4'])
-            ->setAdvantages($data['formData']['advantages'])
-            ->setDisadvantages($data['formData']['disadvantages'])
-            ->setExpertOpinion($data['formData']['expertOpinion'])
-            ->setExpertComment($data['formData']['expertComment']);
-        $this->assertEquals($product, $form->getData());
-        $this->assertEquals($product->getProductCharacteristics(), $form->getData()->getProductCharacteristics());
 
         $view = $form->createView();
         $children = $view->children;
 
-        foreach (array_keys($data['formData']) as $key) {
+        foreach (array_keys($formData) as $key) {
             $this->assertArrayHasKey($key, $children);
         }
     }
@@ -90,30 +107,34 @@ class ProductTypeTest extends AbstractWebCaseTest
     {
         return
             [[
-                 ['categorySlug'    => 'category_slug',
-                  'label1'          => 'label_1',
-                  'label2'          => 'label_2',
-                  'label3'          => 'label_3',
-                  'label4'          => 'label_4',
-                  'product'         => ['name' => 'product_name_test', 'minPrice' => 78.45],
-                  'characteristics' => [
-                      'c_name_1' => ['value' => 100, 'type' => Characteristic::TYPE_INT, 'label' => 'c_name_label_1', 'scale' => 'kg'],
-                      'c_name_2' => ['value' => 'u', 'type' => Characteristic::TYPE_STRING, 'label' => 'c_name_label_2', 'scale' => null],
-                      'c_name_3' => ['value' => 2.58, 'type' => Characteristic::TYPE_DECIMAL, 'label' => 'c_name_label_3', 'scale' => 'cm'],
+                 ['categorySlug'       => 'category_slug',
+                  'label1'             => 'label_1',
+                  'label2'             => 'label_2',
+                  'label3'             => 'label_3',
+                  'label4'             => 'label_4',
+                  'product'            => ['name' => 'product_name_test', 'minPrice' => 78.45],
+                  'characteristics'    => [
+                      ['value' => 100, 'type' => Characteristic::TYPE_INT, 'label' => 'c_name_label_1', 'scale' => 'kg'],
+                      ['value' => 'u', 'type' => Characteristic::TYPE_STRING, 'label' => 'c_name_label_2', 'scale' => null],
+                      ['value' => 2.58, 'type' => Characteristic::TYPE_DECIMAL, 'label' => 'c_name_label_3', 'scale' => 'cm'],
                   ],
-                  'formData'        => ['rating1'                => 11,
-                                        'rating2'                => 22,
-                                        'rating3'                => 33,
-                                        'rating4'                => 44,
-                                        'advantages'             => ['good', 'very good'],
-                                        'disadvantages'          => ['bad', 'very bad'],
-                                        'expertOpinion'          => 'opinion of expert',
-                                        'expertComment'          => 'comment of expert',
-                                        'productCharacteristics' => [
-                                            'c_name_1' => ['valueInt' => 1001],
-                                            'c_name_2' => ['valueString' => 'u'],
-                                            'c_name_3' => ['valueDecimal' => 2.58],
-                                        ],
+                  'new_characteristics' => [
+                      ['type' => Characteristic::TYPE_DECIMAL, 'slug' => 'qwerty', 'label' => 'c_name_label_4', 'scale' => 'cm']
+                  ],
+                  'formData'           => ['rating1'                => 11,
+                                           'rating2'                => 22,
+                                           'rating3'                => 33,
+                                           'rating4'                => 44,
+                                           'advantages'             => ['good', 'very good'],
+                                           'disadvantages'          => ['bad', 'very bad'],
+                                           'expertOpinion'          => 'opinion of expert',
+                                           'expertComment'          => 'comment of expert',
+                                           'productCharacteristics' => [
+                                               ['value' => 1001],
+                                               ['value' => 'u'],
+                                               ['value' => 2.58],
+                                               ['value' => 3.58, 'characteristic' => 'qwerty'],
+                                           ],
                   ]
                  ],
              ]];
