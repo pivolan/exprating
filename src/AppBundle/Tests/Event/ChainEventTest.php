959b21756df51f83e7988f0662163eaa61431628
pivolan <pivolan@gmail.com>
2016-03-03 02:18:01 +0400

    mini refactor test

--- src/AppBundle/Tests/Event/ChainEventTest.php
+++ src/AppBundle/Tests/Event/ChainEventTest.php
@@ -23,26 +23,7 @@ class ChainEventTest extends AbstractWebCaseTest
 {
     public function testPublish()
     {
-        $em = $this->doctrine->getManager();
-        /** @var User $curator */
-        $curator = $em->getRepository('AppBundle:User')->findOneBy(['username' => 'curator']);
-        /** @var User $expert */
-        $expert = $em->getRepository('AppBundle:User')->findOneBy(['username' => 'expert']);
-        $count = $em->getRepository('AppBundle:CuratorDecision')->countNew($curator);
-        $filter = (new ProductFilter())->setCategory($expert->getCategories()[0])->setStatus(
-            ProductFilter::STATUS_FREE
-        );
-        /** @var Product[] $freeProducts */
-        $freeProducts = $em->getRepository('AppBundle:Product')->findByFilterQuery($filter)->getResult();
-        $product = $freeProducts[0];
-        $this->assertFalse($em->getRepository('AppBundle:CuratorDecision')->isExists($product));
-
-        $eventDispatcher = $this->client->getContainer()->get('event_dispatcher');
-        $eventDispatcher->dispatch(ProductEvents::RESERVATION, new ProductReservationEvent($product, $expert));
-
-        $this->assertFalse($em->getRepository('AppBundle:CuratorDecision')->isExists($product));
-        $this->assertEquals($product->getExpertUser(), $expert);
-        $this->assertNotNull($product->getReservedAt());
+        list($em, $curator, $count, $product, $eventDispatcher) = $this->reserveProduct();
 
         $eventDispatcher->dispatch(ProductEvents::PUBLISH_REQUEST, new ProductPublishRequestEvent($product));
 
@@ -64,26 +45,7 @@ class ChainEventTest extends AbstractWebCaseTest
 
     public function testReserveOver()
     {
-        $em = $this->doctrine->getManager();
-        /** @var User $curator */
-        $curator = $em->getRepository('AppBundle:User')->findOneBy(['username' => 'curator']);
-        /** @var User $expert */
-        $expert = $em->getRepository('AppBundle:User')->findOneBy(['username' => 'expert']);
-        $count = $em->getRepository('AppBundle:CuratorDecision')->countNew($curator);
-        $filter = (new ProductFilter())->setCategory($expert->getCategories()[0])->setStatus(
-            ProductFilter::STATUS_FREE
-        );
-        /** @var Product[] $freeProducts */
-        $freeProducts = $em->getRepository('AppBundle:Product')->findByFilterQuery($filter)->getResult();
-        $product = $freeProducts[0];
-        $this->assertFalse($em->getRepository('AppBundle:CuratorDecision')->isExists($product));
-
-        $eventDispatcher = $this->client->getContainer()->get('event_dispatcher');
-        $eventDispatcher->dispatch(ProductEvents::RESERVATION, new ProductReservationEvent($product, $expert));
-
-        $this->assertFalse($em->getRepository('AppBundle:CuratorDecision')->isExists($product));
-        $this->assertEquals($product->getExpertUser(), $expert);
-        $this->assertNotNull($product->getReservedAt());
+        list($em, $curator, $count, $product, $eventDispatcher) = $this->reserveProduct();
 
         $eventDispatcher->dispatch(ProductEvents::RESERVATION_OVER, new ProductReservationOverEvent($product));
         $this->assertNull($product->getExpertUser());
@@ -91,6 +53,33 @@ class ChainEventTest extends AbstractWebCaseTest
     }
 
     public function testReject()
+    {
+        list($em, $curator, $count, $product, $eventDispatcher) = $this->reserveProduct();
+
+        $eventDispatcher->dispatch(ProductEvents::PUBLISH_REQUEST, new ProductPublishRequestEvent($product));
+        /** @var CuratorDecision[] $decisions */
+        $decisions = $em->getRepository('AppBundle:CuratorDecision')->waitByCuratorByProduct($curator, $product)
+            ->getResult();
+        $this->assertCount(1, $decisions);
+        $this->assertTrue($em->getRepository('AppBundle:CuratorDecision')->isExists($product));
+        $countNew = $em->getRepository('AppBundle:CuratorDecision')->countNew($curator);
+        $this->assertEquals($count + 1, $countNew);
+        $eventDispatcher->dispatch(
+            ProductEvents::REJECT,
+            new ProductRejectEvent($product, $curator, 'Плохое описание товара')
+        );
+        $this->assertEquals($count, $em->getRepository('AppBundle:CuratorDecision')->countNew($curator));
+        $this->assertFalse($product->getIsEnabled());
+        $this->assertNotNull($product->getReservedAt());
+        $decision = $em->getRepository('AppBundle:CuratorDecision')->find($decisions[0]->getId());
+        $this->assertEquals(CuratorDecision::STATUS_REJECT, $decision->getStatus());
+        $this->assertEquals('Плохое описание товара', $decision->getRejectReason());
+    }
+
+    /**
+     * @return array
+     */
+    protected function reserveProduct()
     {
         $em = $this->doctrine->getManager();
         /** @var User $curator */
@@ -113,23 +102,6 @@ class ChainEventTest extends AbstractWebCaseTest
         $this->assertEquals($product->getExpertUser(), $expert);
         $this->assertNotNull($product->getReservedAt());
 
-        $eventDispatcher->dispatch(ProductEvents::PUBLISH_REQUEST, new ProductPublishRequestEvent($product));
-        /** @var CuratorDecision[] $decisions */
-        $decisions = $em->getRepository('AppBundle:CuratorDecision')->waitByCuratorByProduct($curator, $product)
-            ->getResult();
-        $this->assertCount(1, $decisions);
-        $this->assertTrue($em->getRepository('AppBundle:CuratorDecision')->isExists($product));
-        $countNew = $em->getRepository('AppBundle:CuratorDecision')->countNew($curator);
-        $this->assertEquals($count + 1, $countNew);
-        $eventDispatcher->dispatch(
-            ProductEvents::REJECT,
-            new ProductRejectEvent($product, $curator, 'Плохое описание товара')
-        );
-        $this->assertEquals($count, $em->getRepository('AppBundle:CuratorDecision')->countNew($curator));
-        $this->assertFalse($product->getIsEnabled());
-        $this->assertNotNull($product->getReservedAt());
-        $decision = $em->getRepository('AppBundle:CuratorDecision')->find($decisions[0]->getId());
-        $this->assertEquals(CuratorDecision::STATUS_REJECT, $decision->getStatus());
-        $this->assertEquals('Плохое описание товара', $decision->getRejectReason());
+        return [$em, $curator, $count, $product, $eventDispatcher];
     }
 }
