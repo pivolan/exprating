0064ab3f08aac1d249e68aa9d96e438d7a422ef9
pivolan <pivolan@gmail.com>
2016-03-04 18:05:26 +0400

    hide menu on click modified.

--- web/js/Exp/exp_hide_menu_on_click.js
+++ web/js/Exp/exp_hide_menu_on_click.js
@@ -2,6 +2,7 @@ Exp.hide_menu_on_click = function (selectorClick) {
     $(document).on('click', selectorClick, function (event) {
         var $this = $(this);
         $this.parent().parent().hide();
-        setTimeout(function(){$this.parent().parent().removeAttr('style')}, 100);
+        $(document).one('mousemove', function(){$this.parent().parent().removeAttr('style')});
+        //setTimeout(function(){$this.parent().parent().removeAttr('style')}, 100);
     });
 };
--- web/js/product_list.init.js
+++ web/js/product_list.init.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    Exp.hide_menu_on_click('#mainSiteMenu a');
+    Exp.hide_menu_on_click('#mainSiteMenu ul.menu_level_1 a');
     Exp.historical(
-        '.rubric-filters ul.dropdown-menu li a, ul.pagination li a, #mainSiteMenu a, .category-child a, .breadcrumbs a',
+        '.rubric-filters a, ul.pagination li a, #mainSiteMenu a, .category-child a, .breadcrumbs a',
         'div.content')
 });
