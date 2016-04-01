08d22b201679558bac42e1bdde0fe696171e5e0e
pivolan <pivolan@gmail.com>
2016-04-01 04:23:48 +0400

    Переделана страница экспертов и страница Мои страницы. Использовано другое дерево. treegrid

--- web/js/curator_my_experts.js
+++ web/js/curator_my_experts.js
@@ -1,17 +1,3 @@
 $(document).ready(function () {
-    var url = $('#experts').data('url');
-    $('#experts').jstree({
-        core: {
-            data: {
-                url: url,
-                data: function (node) {
-                    return {"id": node.id};
-                }
-            }
-        }
-    }).on("select_node.jstree", function (node, selected) {
-        var href = selected.node.a_attr['data-href'];
-        if (href)
-            window.open(href, '_blank');
-    });
+    $('.tree').treegrid();
 });
